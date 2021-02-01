85a075c700ecd69d6cd93db0472f8c4e201ea9ec
Christopher Klint <christopher.klint@gmail.com>
2021-02-01 09:48:57 +0100

    add urls to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,10 @@ export default function Header() {
   return (
     <header className="h-10 pt-2 text-white bg-red-400 ">
       <nav className="flex flex-row justify-center w-1/2 mx-auto">
-        <a className="mx-4" href="#">
+        <a className="mx-4" href="/">
           Home
         </a>
-        <a className="mx-4" href="#">
+        <a className="mx-4" href="https://github.com/christopherklint97/diner">
           Github
         </a>
       </nav>
